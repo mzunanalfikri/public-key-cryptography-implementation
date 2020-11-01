c2d6e8840a403b207cbd6fae16b28e7a9721760e
naufal-dean <naufal.dean@gmail.com>
2020-11-02 03:20:09 +0700

    Implement helper_ui.py

---
+++ src/view/helper_ui.py
@@ -0,0 +1,15 @@
+def spawnDialogWindow(title, text, subtext="" , type="Information"):
+    message = QMessageBox()
+    if type == "Question":
+        message.setIcon(QMessageBox.Question)
+    elif type == "Warning":
+        message.setIcon(QMessageBox.Warning)
+    elif type == "Critical":
+        message.setIcon(QMessageBox.Critical)
+    else:
+        message.setIcon(QMessageBox.Information)
+    message.setWindowTitle(title)
+    message.setText(text)
+    message.setInformativeText(subtext)
+    message.setStandardButtons(QMessageBox.Ok)
+    message._exec()
